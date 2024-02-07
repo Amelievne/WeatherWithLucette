66066b3bcc7812c29f55dce9a3aceb154f8cca08
Amelievne <amelie.vne@gmail.com>
2024-02-07 19:40:55 +0100

    Add API forecast

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ function refreshWeather(response) {
   windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   timeElement.innerHTML = formatDate(date);
   temperatureElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 function temperaturePerceived(temp) {
   if (temp < 10) {
@@ -66,7 +68,20 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
-function displayForecast() {
+
+function getForecast(city) {
+  let apiKey = `974f7528a7fe8f1a738ftd6eob928053`;
+  let units = `metric`;
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+
+  axios(apiUrl).then(displayForecast);
+  console.log(apiUrl);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
   let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
 
@@ -91,4 +106,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Valencia");
-displayForecast();
